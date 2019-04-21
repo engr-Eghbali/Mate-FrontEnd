262733d8f1f8e21b0270e87dec5e04ae1ec97513
reza <reza@>
2019-04-21 16:24:00 +0100

    !#

--- sketch/js/script.js
+++ sketch/js/script.js
@@ -199,8 +199,7 @@ if (document.readyState === 'complete') {
 
 ///////close sideMenu
 function ToggleMenu(){
-
-  
+    
     document.getElementById("profileBTN").classList.toggle("closedMenu");
     setTimeout(function(){document.getElementById("upcomingBTN").classList.toggle("closedMenu");},200);
     setTimeout(function(){document.getElementById("friendBTN").classList.toggle("closedMenu");},250);
