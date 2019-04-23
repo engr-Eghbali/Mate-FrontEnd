e51b9d796bc1fdcc86c00b07735d355b599d1258
reza <reza@>
2019-04-23 11:22:04 +0100

    submitPhone page UI implemented

--- sketch/js/navigation.js
+++ sketch/js/navigation.js
@@ -196,14 +196,3 @@ if (document.readyState === 'complete') {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-///////close sideMenu
-function ToggleMenu(){
-    
-    document.getElementById("profileBTN").classList.toggle("closedMenu");
-    setTimeout(function(){document.getElementById("upcomingBTN").classList.toggle("closedMenu");},200);
-    setTimeout(function(){document.getElementById("friendBTN").classList.toggle("closedMenu");},250);
-    setTimeout(function(){document.getElementById("pendingReqBTN").classList.toggle("closedMenu");},300);
-    setTimeout(function(){document.getElementById("setMeetingBTN").classList.toggle("closedMenu");},350);
-    setTimeout(function(){document.getElementById("sideBar").classList.toggle("hidden")},500)
-}
