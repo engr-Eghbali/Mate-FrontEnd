09af28a065acf3192ca3948722a377d9aa025623
reza <reza@>
2019-04-23 14:33:35 +0100

    VC page implemented

--- sketch/js/paging.js
+++ sketch/js/paging.js
@@ -2,10 +2,8 @@
 document.onreadystatechange = () => {
 
     if (document.readyState === 'complete') {
-    
-    
-        }   
-    };
+    }
+};
     
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -20,4 +18,24 @@ document.onreadystatechange = () => {
         setTimeout(function(){document.getElementById("pendingReqBTN").classList.toggle("closedMenu");},300);
         setTimeout(function(){document.getElementById("setMeetingBTN").classList.toggle("closedMenu");},350);
         setTimeout(function(){document.getElementById("sideBar").classList.toggle("hidden")},500)
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    function submitPhone(){
+
+        document.getElementById("uidForm").style.left=-100+"vh";
+        setTimeout(function(){document.getElementById("uidForm").style.display="none"},1500);
+
+      
+    }
+
+    function submitVC(){
+
+        document.getElementById("verifyForm").style.left=-100+"vh";
+        setTimeout(function(){document.getElementById("verifyForm").style.display="none"},1500);
+
+      
     }
